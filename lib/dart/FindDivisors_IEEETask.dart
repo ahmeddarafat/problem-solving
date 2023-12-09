5c9b352b3fcbfe4be5905ecf57363ccf6ff8a3af
Ahmed Arafat <ahmed.arafat3535@gmail.com>
2023-12-09 17:12:20 +0200

    Update new solution for FindDivisors_IEEETask.dart

--- lib/dart/FindDivisors_IEEETask.dart
+++ lib/dart/FindDivisors_IEEETask.dart
@@ -1,11 +1,13 @@
 import 'dart:io';
+import 'dart:math';
 
 void main() {
   print(findDivisors());
+  print(findDivisors2());
 }
 
 List<int> findDivisors() {
-  print("Please enter a number:");
+  stdout.write("Please enter a number: ");
   String? input = stdin.readLineSync();
 
   int? number;
@@ -25,4 +27,29 @@ List<int> findDivisors() {
   }
 
   return divisors;
+}
+
+/// another solution
+List<int> findDivisors2() {
+  stdout.write("Please enter a number: ");
+  String? input = stdin.readLineSync();
+
+  int? number;
+  if (input != null) {
+    number = int.tryParse(input);
+    if (number == null) {
+      print("Please enter a valid number.");
+      return List.empty();
+    }
+  }
+
+  List<int> divisors = [1, number!];
+  for (int i = 2; i < sqrt(number) + 1; i++) {
+    if (number % i == 0) {
+      divisors.add(i);
+      divisors.add((number ~/ i));
+    }
+  }
+
+  return divisors.toSet().toList();
 }
